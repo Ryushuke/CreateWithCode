41370e2d9d27f6f3a41f1512c7fa22f4309078d0
Ryushuke <ryushuke.dev@gmail.com>
2022-08-11 16:34:06 -0300

    Implementado um pool para os projeteis.

---
+++ Prototype 2/Assets/Scripts/Projectile/ProjectilePool.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.Pool;
+
+namespace Game.Projectile
+{
+	[AddComponentMenu("Game/Projeteis/Pool de Projeteis")]
+	public class ProjectilePool : MonoBehaviour
+	{
+		[SerializeField]
+		private GameObject _projectile;
+		[Min(1)]
+		[SerializeField]
+		private int _capacity;
+
+		private ObjectPool<GameObject> pool;
+
+		public GameObject Get() => pool.Get();
+
+		private void Awake()
+		{
+			pool = new ObjectPool<GameObject>(Create,
+				actionOnRelease: OnRelease,
+				defaultCapacity: _capacity);
+		}
+
+		private GameObject Create()
+		{
+			GameObject go = Instantiate(_projectile, transform);
+			go.name = string.Format("{0} ({1:00})",
+				_projectile.name,
+				pool.CountAll);
+			return go;
+		}
+
+		private void OnRelease(GameObject projectile)
+		{
+			projectile.transform.SetPositionAndRotation(transform.position, transform.rotation);
+		}
+	}
+}
