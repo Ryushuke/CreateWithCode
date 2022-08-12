773772284e125206ec177c69970697cf9a7cd7fe
Ryushuke <ryushuke.dev@gmail.com>
2022-08-12 10:13:49 -0300

    Implementado um ponto de spawn para os inimigos.
    - A posição x pode variar entre um minimo e um máximo.

---
+++ Prototype 2/Assets/Scripts/Enemies/SpawnPoint.cs
@@ -0,0 +1,34 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Game.Enemies
+{
+	[AddComponentMenu("Game/Inimigos/Ponto de Inserção")]
+	public class SpawnPoint : MonoBehaviour
+	{
+		[SerializeField]
+		private float _xMax;
+		[SerializeField]
+		private float _xMin;
+
+		public Vector3 GetPosition()
+		{
+			float x = Random.Range(_xMin, _xMax);
+			return new Vector3(x, transform.position.y, transform.position.z);
+		}
+
+		public Quaternion GetRotation()
+		{
+			return transform.rotation;
+		}
+
+		private void OnDrawGizmos()
+		{
+			Vector3 a = transform.position;
+			Vector3 b = a;
+			a.x = _xMin;
+			b.x = _xMax;
+			Gizmos.DrawLine(a, b);
+		}
+	}
+}
