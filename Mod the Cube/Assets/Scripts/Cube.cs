ec39643bc7b871bd5ffd2fbe2945bebfee785173
Ryushuke <ryushuke.dev@gmail.com>
2022-08-23 14:52:11 -0300

    Alterado comportamento do cubo.
    - Removendo o reposicionamento do cubo ao iniciar.

--- Mod the Cube/Assets/Scripts/Cube.cs
+++ Mod the Cube/Assets/Scripts/Cube.cs
@@ -86,7 +86,6 @@ public class Cube : MonoBehaviour
 
 	private void Start()
 	{
-		transform.position = new Vector3(3, 4, 1);
 		transform.localScale = Vector3.one * 1.3f;
 		Material material = Renderer.material;
 		material.color = new Color(0.5f, 1.0f, 0.3f, 0.4f);
