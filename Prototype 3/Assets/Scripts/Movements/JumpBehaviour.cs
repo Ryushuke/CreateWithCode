877e6fdd16b7f9951f962aac31e7880b3753647c
Ryushuke <ryushuke.dev@gmail.com>
2022-08-25 19:05:41 -0300

    Alterado o Pulo.
    - Evitando a possibilidade de dar pulos duplos.

--- Prototype 3/Assets/Scripts/Movements/JumpBehaviour.cs
+++ Prototype 3/Assets/Scripts/Movements/JumpBehaviour.cs
@@ -10,9 +10,14 @@ namespace Game.Movements
 
 		private Rigidbody rigidBody;
 
+		public bool CanJump { get; set; } = true;
+
 		[ContextMenu("Pular")]
 		public void Jump()
 		{
+			if (!CanJump)
+				return;
+
 			Vector3 force = Vector3.up * _jumpForce;
 			rigidBody.AddForce(force, ForceMode.Impulse);
 		}
